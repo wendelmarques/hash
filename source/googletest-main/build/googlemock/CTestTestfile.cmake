0043772d76669dddd2735b6f995dced8d593d7a7
Wendel Marques <wendelmjs@discente.ufg.br>
2023-06-29 22:11:19 -0300

    Add files via upload

---
+++ source/googletest-main/build/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/c/Users/bcbru/OneDrive/Documentos/trabalho-05-hash-table-cansadas-master/source/googletest-main/googlemock
+# Build directory: /mnt/c/Users/bcbru/OneDrive/Documentos/trabalho-05-hash-table-cansadas-master/source/googletest-main/build/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
